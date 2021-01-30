73f111c7211475377c916659494f7467e443ee18
Công Sơn <71420045+congsonzero@users.noreply.github.com>
2021-01-30 13:42:10 +0700

    swagger

--- gymho/urls.py
+++ gymho/urls.py
@@ -18,14 +18,22 @@ from django.contrib import admin
 from django.conf.urls import url, include 
 from django.views.generic import TemplateView
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+
 import os
 urlpatterns = [
    
     url(r'^admin/', admin.site.urls),
     url(r'^', include('scheduler.urls')),
+
     path('swagger-ui/', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url':'openapi-schema'}
     ), name='swagger-ui'),
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
 
--- scheduler/views.py
+++ scheduler/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from django.template import loader
+
 
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
